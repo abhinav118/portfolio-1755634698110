a2800f1d3060814dcad1ac33a253598151d93c0f
Abhi <abhinav118@gmail.com>
2025-08-19 16:18:24 -0400

    Add components/DarkModeToggle.tsx

---
+++ components/DarkModeToggle.tsx
@@ -0,0 +1,12 @@
+interface DarkModeToggleProps {
+  variant?: any
+  size?: any
+}
+
+export default function DarkModeToggle({ variant, size }: DarkModeToggleProps) {
+  return (
+    <div className="">
+      {/* DarkModeToggle component content */}
+    </div>
+  )
+}
