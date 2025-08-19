bac8e61209bbf20d553cb3bd6978a6b6218d5d4e
Abhi <abhinav118@gmail.com>
2025-08-19 16:18:25 -0400

    Add components/Navbar.tsx

---
+++ components/Navbar.tsx
@@ -0,0 +1,12 @@
+interface NavbarProps {
+  fixed?: any
+  transparent?: any
+}
+
+export default function Navbar({ fixed, transparent }: NavbarProps) {
+  return (
+    <div className="">
+      {/* Navbar component content */}
+    </div>
+  )
+}
