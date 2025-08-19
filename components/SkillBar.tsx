24feec85e7710bf014f5932bf2e2c85d17962740
Abhi <abhinav118@gmail.com>
2025-08-19 16:18:25 -0400

    Add components/SkillBar.tsx

---
+++ components/SkillBar.tsx
@@ -0,0 +1,12 @@
+interface SkillBarProps {
+  showPercentage?: any
+  animated?: any
+}
+
+export default function SkillBar({ showPercentage, animated }: SkillBarProps) {
+  return (
+    <div className="">
+      {/* SkillBar component content */}
+    </div>
+  )
+}
