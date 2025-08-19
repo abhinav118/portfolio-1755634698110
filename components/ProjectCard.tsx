caaed555075ea1cecc65a2d4a56418e14dede70a
Abhi <abhinav118@gmail.com>
2025-08-19 16:18:24 -0400

    Add components/ProjectCard.tsx

---
+++ components/ProjectCard.tsx
@@ -0,0 +1,13 @@
+interface ProjectCardProps {
+  showImage?: any
+  showTags?: any
+  showLinks?: any
+}
+
+export default function ProjectCard({ showImage, showTags, showLinks }: ProjectCardProps) {
+  return (
+    <div className="">
+      {/* ProjectCard component content */}
+    </div>
+  )
+}
